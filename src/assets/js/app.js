04ae5f25d9ab13be18f7db041875039e811d9793
aeriel999 <ok4743@gmail.com>
2023-06-19 08:17:25 +0300

    Add project files.

---
+++ src/assets/js/app.js
@@ -0,0 +1,16 @@
+fun = (a) => {
+    return a;
+};
+
+$(".autoplay").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 7000,
+});
+$(".autoplay_2").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 10000,
+});
